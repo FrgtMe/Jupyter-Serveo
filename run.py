b606350e01bea2a81920ab5a2830950d2a5dacb2
FrgtMe <142883038+FrgtMe@users.noreply.github.com>
2024-05-29 22:23:51 +0300

    Update run.py

--- run.py
+++ run.py
@@ -8,5 +8,5 @@ def run_jupyter():
   os.system("echo -e 'e\ne' | jupyter notebook password && jupyter notebook --ip=0.0.0.0 --port=3000 --allow-root --no-browser")
 
 if True:
-  Thread(target=run_serveo).start();
   Thread(target=run_jupyter).start();
+  Thread(target=run_serveo).start();
