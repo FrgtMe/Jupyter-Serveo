8c24b4a35c70ca82b52686253c59a70f729be94f
FrgtMe <142883038+FrgtMe@users.noreply.github.com>
2024-05-30 16:59:06 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from threading import Thread
 import os
 
 def run_serveo():
-  os.system("bash serveo.sh")
+  os.system("npm i -g localtunnel && bash serveo.sh")
 
 def run_jupyter():
   os.system("echo -e 'e\ne' | jupyter notebook password && jupyter notebook --ip=0.0.0.0 --port=3000 --allow-root --no-browser")
